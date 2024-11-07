f05fe60f8fc5b29dc97c5a2466a5132e82f78002
RizkyTaba <rizkitaba56@gmail.com>
2024-11-07 12:49:42 +0700

    first commit

---
+++ src/models/Book.js
@@ -0,0 +1,9 @@
+export class Book {
+  constructor(id, title, author, year) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.author = author;
+    this.year = year;
+  }
+}
